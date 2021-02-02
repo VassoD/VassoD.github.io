7bd040668290d05d13a2adef8ef62b26ed9665ca
VassoD <vassodor@gmail.com>
2021-02-02 17:50:51 +0200

    ..

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("header");
-    if (x.className === "inner_header") {
-      x.className += "responsive";
-    } else {
-      x.className = "inner_header";
-    }
-  }
 
 /* alert("Salut, c'est un quiz")*/
 function myFunction()
 function vrai(questionNumber) {
-    document.getElementById("reponse" + questionNumber).innerHTML = "vrai";
+    document.getElementById("reponse1" + questionNumber).innerHTML = "vrai";
 }
 function faux(questionNumber) {
-    document.getElementById("reponse" + questionNumber).innerHTML = 'faux';
+    document.getElementById("reponse2" + questionNumber).innerHTML = 'faux';
+}
+
+function myFunction() {
+  var x = document.getElementById("header");
+  if (x.className === "inner_header") {
+    x.className += "responsive";
+  } else {
+    x.className = "inner_header";
+  }
 }
