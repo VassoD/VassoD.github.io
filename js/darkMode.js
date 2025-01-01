ea37f75171a400675ae66bf47ca7f674387f0426
Vasiliki Doropoulou <vasiliki@ublo.immo>
2025-01-01 21:50:30 +0100

    fix: darkmode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,6 +1,6 @@
-document.querySelector(".nightmode").addEventListener("click", () => {
+document.querySelector(".darkMode").addEventListener("click", () => {
   document.querySelector(".moon-logo").classList.toggle("animate-moon");
   document.querySelector(".sun-logo").classList.toggle("animate-sun");
   document.querySelector("body").classList.toggle("dark");
-  document.querySelector(".nightmode").classList.toggle("dark");
+  document.querySelector(".darkMode").classList.toggle("dark");
 });
